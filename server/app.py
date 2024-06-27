0bd3ddfd785b320efb1c86600cd80df0277b5183
Linet <teweilinet@gmail.com>
2024-06-27 13:48:46 +0300

    task3-view to get earthquake by id

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask,jsonify, make_response
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -21,7 +21,22 @@ def index():
     return make_response(body, 200)
 
 # Add views here
-
+@app.route('/earthquakes/<int:id>', methods=['GET'])
+def main(id):
+    earthquake=Earthquake.query.get(id)
+    if earthquake is None:
+        response_body= {"message":f'Earthquake {id} not found.'}
+        status=404
+    else:
+     response_body= {'id':earthquake.id,
+                      'location':earthquake.location,
+                      'magnitude':earthquake.magnitude,
+                      'year':earthquake.year
+                    }
+     status=200
+    return make_response(jsonify(response_body),status)
+    
+    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
